cf629ee5d03d95ec984aed5ae055df685e004357
pvdo <fire_ra@bk.ru>
2024-10-18 18:48:16 +0300

    from notification can open app with variable

--- app/src/main/java/com/vk/directop/type_safedeeplinking/MainActivity.kt
+++ app/src/main/java/com/vk/directop/type_safedeeplinking/MainActivity.kt
@@ -4,18 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navDeepLink
+import androidx.navigation.toRoute
 import com.vk.directop.type_safedeeplinking.ui.theme.TypesafeDeepLinkingTheme
 import kotlinx.serialization.Serializable
+
+const val DEEPLINK_DOMAIN = "my-app-deeplink"
 
 @Serializable
 data object HomeScreen
@@ -40,8 +46,20 @@ class MainActivity : ComponentActivity() {
                         composable<HomeScreen> {
                             HomeScreen()
                         }
-                        composable<DeepLinkScreen> {
-
+                        composable<DeepLinkScreen>(
+                            deepLinks = listOf(
+                                navDeepLink<DeepLinkScreen>(
+                                    basePath = "https://$DEEPLINK_DOMAIN"
+                                )
+                            )
+                        ) {
+                            val id = it.toRoute<DeepLinkScreen>().id
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center,
+                            ) {
+                                Text(text = "The ID is $id")
+                            }
                         }
                     }
                 }
--- app/src/main/java/com/vk/directop/type_safedeeplinking/MyDeepLinkApp.kt
+++ app/src/main/java/com/vk/directop/type_safedeeplinking/MyDeepLinkApp.kt
@@ -5,9 +5,12 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.app.TaskStackBuilder
 import androidx.core.content.getSystemService
+import androidx.core.net.toUri
 
 class MyDeepLinkApp : Application() {
 
@@ -17,6 +20,7 @@ class MyDeepLinkApp : Application() {
         showNotification()
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel() {
         val channel = NotificationChannel(
             CHANNEL_ID,
@@ -28,7 +32,9 @@ class MyDeepLinkApp : Application() {
     }
 
     private fun showNotification() {
-        val activityIntent = Intent(this, MainActivity::class.java)
+        val activityIntent = Intent(this, MainActivity::class.java).apply {
+            data = "https://$DEEPLINK_DOMAIN/8457".toUri()
+        }
         val pendingIntent = TaskStackBuilder.create(this).run {
             addNextIntentWithParentStack(activityIntent)
             getPendingIntent(0, PendingIntent.FLAG_IMMUTABLE)
